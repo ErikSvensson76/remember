73100b00b06224970d48776014ce24e8513921d7
ErikSvensson76 <eosvensson@gmail.com>
2024-11-14 18:36:04 +0100

    Updated ViewDate to only have one date field.

--- src/main/java/org/example/remember/model/dto/ViewDate.java
+++ src/main/java/org/example/remember/model/dto/ViewDate.java
@@ -8,38 +8,25 @@ import java.util.Objects;
 public class ViewDate implements Serializable {
 
   private final LocalDate date;
-  private final Integer month;
-  private final Integer year;
 
-  public ViewDate(LocalDate date, Integer month, Integer year) {
+  public ViewDate(LocalDate date) {
     this.date = date;
-    this.month = month;
-    this.year = year;
   }
 
   public Integer getYear(){
-    return year;
+    return date.getYear();
+  }
+
+  public Integer getDayOfWeek(){
+    return date.getDayOfMonth();
+  }
+
+  public Integer getMonth(){
+    return date.getMonthValue();
   }
 
   public Integer getWeekNumber(){
     return date.get(WeekFields.ISO.weekOfWeekBasedYear());
-  }
-
-  public boolean isGrayedOut(){
-    return date.getMonthValue() != month;
-  }
-
-  public String getDay(){
-    return switch (date.getDayOfWeek().getValue()) {
-      case 1 -> "Måndag";
-      case 2 -> "Tisdag";
-      case 3 -> "Onsdag";
-      case 4 -> "Torsdag";
-      case 5 -> "Fredag";
-      case 6 -> "Lördag";
-      case 7 -> "Söndag";
-      default -> throw new IllegalArgumentException("Invalid day");
-    };
   }
 
   @Override
