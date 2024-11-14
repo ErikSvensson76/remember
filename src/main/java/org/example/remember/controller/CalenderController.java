be2bf9ca8c1220ab8542a82f8d20506ce85b7876
ErikSvensson76 <eosvensson@gmail.com>
2024-11-14 18:39:01 +0100

    Updated CalenderController

--- src/main/java/org/example/remember/controller/CalenderController.java
+++ src/main/java/org/example/remember/controller/CalenderController.java
@@ -13,7 +13,6 @@ import java.time.YearMonth;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/calender")
@@ -32,31 +31,51 @@ public class CalenderController {
       @RequestParam(name = "month", required = false) Integer month) {
     Optional<Integer> optionalYear = Optional.ofNullable(year);
     Optional<Integer> optionalMonth = Optional.ofNullable(month);
-    List<ViewDate> dateWithWeekList;
+    Map<Integer, List<ViewDate>> dateWithWeekList;
+
+    int y;
+    int m;
 
     if(optionalYear.isPresent() && optionalMonth.isPresent()) {
-      dateWithWeekList = calenderService.getMonth(optionalYear.get(),optionalMonth.get()).get();
-
+      y = optionalYear.get();
+      m = optionalMonth.get();
     }else if(optionalYear.isPresent()){
-      int y = optionalYear.get();
-      int m = YearMonth.of(y, 1).getMonthValue();
-      dateWithWeekList = calenderService.getMonth(y, m).get();
+      y = optionalYear.get();
+      m = YearMonth.of(y, 1).getMonthValue();
     }else if(optionalMonth.isPresent()){
-      int y = optionalMonth.get();
-      int m = Year.now().getValue();
-      dateWithWeekList = calenderService.getMonth(y, m).get();
+      y = optionalMonth.get();
+      m = Year.now().getValue();
     }else {
-      int y = Year.now().getValue();
-      int m = YearMonth.of(y, 1).getMonthValue();
-      dateWithWeekList = calenderService.getMonth(y, m).get();
+      y = Year.now().getValue();
+      m = YearMonth.of(y, 1).getMonthValue();
     }
+    dateWithWeekList = calenderService.getMonth(y, m).get();
 
-    Map<Integer,List<ViewDate>> map = dateWithWeekList.stream()
-            .collect(Collectors.groupingBy(ViewDate::getWeekNumber));
+    model.addAttribute("dates", dateWithWeekList);
+    model.addAttribute("year", y);
+    model.addAttribute("month", m);
+    model.addAttribute("title", getMonth(m)+" "+y);
 
-
-    model.addAttribute("dates", map);
     return "calender";
   }
+
+  public String getMonth(int month){
+      return switch (month){
+        case 1 -> "Januari";
+        case 2 -> "Februari";
+        case 3 -> "Mars";
+        case 4 -> "April";
+        case 5 -> "Maj";
+        case 6 -> "Juni";
+        case 7 -> "Juli";
+        case 8 -> "Augusti";
+        case 9 -> "September";
+        case 10 -> "Oktober";
+        case 11 -> "November";
+        case 12 -> "December";
+        default -> throw new IllegalStateException("Unexpected value: " + month);
+      };
+  }
+
 
 }
