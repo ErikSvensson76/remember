7c663053a98141ea0bb455c566dab27a8164b50e
ErikSvensson76 <eosvensson@gmail.com>
2024-11-21 14:35:12 +0100

    Added EntityUtils in order to avoid hardcoding

---
+++ src/main/java/org/example/remember/model/entity/EntityUtils.java
@@ -0,0 +1,14 @@
+package org.example.remember.model.entity;
+
+public class EntityUtils {
+  public static final String TODO_ITEM_TABLE = "todo_items";
+  public static final String TODO_ITEM_ID = "pk_todo_item";
+  public static final String TODO_ITEM_TITLE = "title";
+  public static final String TODO_ITEM_DESCRIPTION = "description";
+  public static final String TODO_ITEM_PRIORITY = "priority";
+  public static final String TODO_ITEM_DEADLINE = "deadline";
+  public static final String TODO_ITEM_DONE = "done";
+
+  private EntityUtils(){}
+}
+
